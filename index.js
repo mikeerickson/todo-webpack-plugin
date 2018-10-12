09540276cbd9e01d8b31d68a3443c5a2b517e2bd
Mike Erickson <codedungeon@gmail.com>
2018-10-11 17:10:36 -0700

    Fixed #16

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 /* global require, module */
 
-const path   = require('path');
-const fs     = require('fs');
+const path = require('path');
+const fs = require('fs');
 const leasot = require('leasot');
-const slash  = require('slash');
+const slash = require('slash');
 
 const DEFAULT_OPTIONS = {
-  console:            true,       // default true
-  tags:               [],         // default TODO
-  reporter:           'markdown', // default markdown
-  skipUnsupported:    true,       // skip unsupported files
+  console: true,       // default true
+  tags: [],         // default TODO
+  reporter: 'markdown', // default markdown
+  skipUnsupported: true,       // skip unsupported files
   suppressFileOutput: false,      // don't output to file,
-  relativeFilePath:   true,       // display relative file paths in report
-  withInlineFiles:    false       // parse possible inline files
+  relativeFilePath: true,       // display relative file paths in report
+  withInlineFiles: false       // parse possible inline files
 };
 
 function TodoWebpackPlugin(options) {
@@ -22,69 +22,69 @@ function TodoWebpackPlugin(options) {
 TodoWebpackPlugin.prototype = {
   constructor: TodoWebpackPlugin,
 
-  apply: function (compiler) {
-    compiler.plugin('done', (compilation, callback) => {
+  apply:  (compiler) => {
+    compiler.hooks.done.tap('TodoWebpackPlugin', params => {
       return reporter(this.pluginOpts, compiler._lastCompilationFileDependencies);
     });
   }
 };
 
 function reporter(options, files) {
-    let todos  = [];
-    let output = '';
+  let todos = [];
+  let output = '';
 
-    files.forEach(file => {
-      if (/node_modules|bower_components|vendor/.test(file)) {
-        return; // skip node modules
+  files.forEach(file => {
+    if (/node_modules|bower_components|vendor/.test(file)) {
+      return; // skip node modules
+    }
+    if (options.skipUnsupported) {
+      if (!leasot.isExtSupported(path.extname(file))) {
+        return;
       }
-      if (options.skipUnsupported) {
-        if (!leasot.isExtSupported(path.extname(file))) {
-          return;
-        }
-      }
-      const todo = leasot.parse({
-        ext:              path.extname(file),
-        content:          fs.readFileSync(file, 'utf8'),
-        fileName:         file,
-        customTags:       options.tags,
-        reporter:         options.reporter,
-        withInlineFiles:  options.withInlineFiles
-      });
-      todos = todos.concat(todo);
+    }
+    const todo = leasot.parse({
+      ext: path.extname(file),
+      content: fs.readFileSync(file, 'utf8'),
+      fileName: file,
+      customTags: options.tags,
+      reporter: options.reporter,
+      withInlineFiles: options.withInlineFiles
     });
+    todos = todos.concat(todo);
+  });
 
-    if (options.console) {
-      console.log('\n');
-      output = leasot.reporter(todos, {reporter: 'table', spacing: 2});
+  if (options.console) {
+    console.log('\n');
+    output = leasot.reporter(todos, {reporter: 'table', spacing: 2});
+    if (options.relativeFilePath) {
+      output = relativePath(output);
+    }
+    console.log(output); // eslint-disable-line
+    console.log('\n\n');
+  }
+
+  output = leasot.reporter(todos, {reporter: options.reporter, spacing: 2});
+
+  if (output.length > 0) {
+    let outputFilename = options.filename || '';
+    if (outputFilename.length === 0) {
+      outputFilename = (options.reporter === 'markdown') ? 'TODO.md' : 'todo.' + options.reporter;
+      if (options.reporter === 'table') {
+        outputFilename = 'todo.txt';
+      }
+    }
+
+    if (!options.suppressFileOutput) {
       if (options.relativeFilePath) {
         output = relativePath(output);
       }
-      console.log(output); // eslint-disable-line
-      console.log('\n\n');
+      fs.writeFile(outputFilename, output, (err) => {
+        if (err) throw err;
+      });
     }
+  }
 
-    output = leasot.reporter(todos, {reporter: options.reporter, spacing: 2});
-
-  if (output.length > 0) {
-      let outputFilename = options.filename || '';
-      if (outputFilename.length === 0) {
-        outputFilename = (options.reporter === 'markdown') ? 'TODO.md' : 'todo.' + options.reporter;
-        if (options.reporter === 'table') {
-          outputFilename = 'todo.txt';
-        }
-      }
-
-      if (!options.suppressFileOutput) {
-        if (options.relativeFilePath) {
-          output = relativePath(output);
-        }
-        fs.writeFile(outputFilename, output, (err) => {
-          if (err) throw err;
-        });
-      }
-    }
-
-    return true;
+  return true;
 
 }
 
