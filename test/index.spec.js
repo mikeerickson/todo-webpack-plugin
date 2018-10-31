37268f5b62f7ad1ffa8f24c8e27630aacc63dc7c
Mike Erickson <codedungeon@gmail.com>
2018-10-31 13:40:13 -0700

    updated tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,26 +1,26 @@
 /* eslint no-console: 0 */
 
-import fs from 'fs';
-import test from 'ava';
-import path from 'path';
-import webpack from 'webpack';
-import chalk from 'chalk';
+import fs from "fs";
+import test from "ava";
+import path from "path";
+import webpack from "webpack";
+import chalk from "chalk";
 
 // 0. Import the config that uses my plugin
-import options from '../webpack.config.js';
+import options from "../webpack.config.js";
 
-let todoFilename = '';
+let todoFilename = "";
 test.beforeEach(t => {
-  todoFilename = path.join(__dirname, '..', 'TODO.md');
+  todoFilename = path.join(__dirname, "..", "TODO.md");
 });
 
-test.cb(chalk.cyan.bold('Creates ./TODO.md file'), t => {
-  webpack(options, function (err, stats) {
-    const todoFilename = path.join(__dirname, '..', 'TODO.md');
+test.cb(chalk.cyan.bold("Creates ./TODO.md file"), t => {
+  webpack(options, function(err, stats) {
+    const todoFilename = path.join(__dirname, "..", "TODO.md");
+    console.log(err);
     if (err) {
       return t.end(err);
-    }
-    else if (stats.hasErrors()) {
+    } else if (stats.hasErrors()) {
       return t.end(stats.toString());
     }
 
@@ -29,25 +29,22 @@ test.cb(chalk.cyan.bold('Creates ./TODO.md file'), t => {
     t.end();
   });
   t.pass();
-
 });
 
-test.cb(chalk.cyan.bold('Verify TODO.md file contains items'), t => {
+test.cb.only(chalk.cyan.bold("Verify TODO.md file contains items"), t => {
   if (fs.existsSync(todoFilename)) {
-    let buffer = fs.readFileSync(todoFilename, 'utf8');
+    let buffer = fs.readFileSync(todoFilename, "utf8");
 
     // make sure TODO section is created
-    t.true(buffer.includes('### TODOs'));
-    t.true(buffer.includes('Sample Todo'));
+    t.true(buffer.includes("### TODOs"));
+    t.true(buffer.includes("Sample Todo"));
 
     // make sure FIXME section is created
-    t.true(buffer.includes('### FIXME'));
-    t.true(buffer.includes('Sample Fixme'));
-  }
-  else {
-    console.log(chalk.red.bold(' 🚫  Unable to locate ./TODO.md file'));
+    t.true(buffer.includes("### FIXME"));
+    t.true(buffer.includes("Sample Fixme"));
+  } else {
+    console.log(chalk.red.bold(" 🚫  Unable to locate ./TODO.md file"));
     t.pass();
   }
   t.end();
 });
-
